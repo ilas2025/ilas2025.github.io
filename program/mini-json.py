841bfc6007bcb40e8bb45036e3c7773239de7680
Jephian Lin <jephianlin@gmail.com>
2024-12-29 16:40:21 +0800

    switch csv to json data for  minis

--- program/mini-json.py
+++ program/mini-json.py
@@ -9,12 +9,11 @@ mini_all = minis.copy()
 sdata = []
 for SID in minis.index:
     mask = (mini_speakers.TYPE == SID)
-    query = mini_speakers.loc[mask, ["NAME", "TITLE"]].to_json(orient="records")
+    query = mini_speakers.loc[mask, ["NAME", "TITLE"]].to_json(orient="records", force_ascii=False)
     sdata.append(json.loads(query))
 
 mini_all["SPEAKERS"] = sdata
-mini_all.reset_index(inplace=True)
-mini_all_json = mini_all.to_json(orient="records")
-mini_all_json = json.dumps(json.loads(mini_all_json), indent=4)
-with open("mini-all.json", "w") as f:
+# mini_all.reset_index(inplace=True)
+mini_all_json = mini_all.to_json(orient="index", force_ascii=False, indent=4)
+with open("mini-all.json", "w", encoding='utf-8') as f:
     f.write(mini_all_json)
